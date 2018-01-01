d254cbe46812ba06fe88d4b4313a415dca7a474d
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-01 01:29:24 +0100

    Ajout listener & message start

--- src/main/java/fr/mrfern/pumpmycord/Main.java
+++ src/main/java/fr/mrfern/pumpmycord/Main.java
@@ -4,13 +4,11 @@ import java.util.HashMap;
 
 import fr.mrfern.pumpmycord.config.Config;
 import fr.mrfern.pumpmycord.porg.MisterPorg;
-import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class Main extends Plugin {
     
-	public JDA jda;
 	public TextChannel defaultBorgChan;
 	public HashMap<String,TextChannel> hashChannel = new HashMap<>();
 	private MisterPorg misterP;
@@ -18,14 +16,25 @@ public class Main extends Plugin {
 	
 	@Override
 	public void onLoad() {
-		setMisterP(new MisterPorg(this, "MzgyNTc4Mzg4MDY3NTQ5MTg0.DQdApA.zxYqzecf2pn3HMt6rRZGbcibggs"));	
+		setMisterP(new MisterPorg(this, "MzgyNTc4Mzg4MDY3NTQ5MTg0.DQdApA.zxYqzecf2pn3HMt6rRZGbcibggs"));
+		getMisterP().addListener(new ReceiveMessageEvent());
+		
+		defaultBorgChan = misterP.getJda().getTextChannelById("387326167499276292");
+		
+		defaultBorgChan.sendMessage("bungee proxy started").complete().pin().complete();
+		defaultBorgChan.sendMessage("bungee proxy started").complete().pin().complete();
+		defaultBorgChan.sendMessage("bungee proxy started").complete().pin().complete();
 	}
 	
 	@Override
     public void onEnable() {
+		
         Config conf = Config.getConfig(this);
         
-        conf.initFile("");
+        conf.initDataFolder();
+        
+        conf.initAndGetFile("reboot_message_history.yml");
+        conf.initAndGetFile("reboot_message.yml");
     }
 
 	public MisterPorg getMisterP() {
