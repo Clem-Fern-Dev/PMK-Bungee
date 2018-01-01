246b6f9d9c6aa3a7c6c51e91102154767bb4a3e9
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-01 01:28:11 +0100

    ajout methods d'initialisation de fichier de config et récupération de l'instance Configuration

--- src/main/java/fr/mrfern/pumpmycord/config/Config.java
+++ src/main/java/fr/mrfern/pumpmycord/config/Config.java
@@ -1,8 +1,14 @@
 package fr.mrfern.pumpmycord.config;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 import fr.mrfern.pumpmycord.Main;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
 
 public class Config {
 
@@ -14,10 +20,39 @@ public class Config {
 		return config;
 	}
 
-	public void initFile(String path) {
+	public void initDataFolder() {
 		
-		File file = new File(path);
+		if(!main.getDataFolder().exists()) {
+			main.getDataFolder().mkdir();
+		}
 		
+	}
+	
+	public File initAndGetFile(String fileName) {
+		
+		File file = new File(main.getDataFolder(),fileName);
+		
+		if(!file.exists()) {
+			try (InputStream in = main.getResourceAsStream(fileName)){
+				Files.copy(in, file.toPath());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return file;
+	}
+	
+	public Configuration getConfiguration(String fileName) throws Exception {
+		
+		File file = new File(main.getDataFolder(),fileName);
+		
+		try {
+			return ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
+		} catch (Exception e) {
+			throw new Exception( fileName + " impossible de récupérer la configuration" );
+		}		
 	}
 	
 }
