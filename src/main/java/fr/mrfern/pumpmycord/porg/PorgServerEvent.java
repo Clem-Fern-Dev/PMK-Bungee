528af44fe69a0e33972a46f1c941d2479960eec1
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-08 19:16:02 +0100

    Ajout event ConnectedServerEvent

---
+++ src/main/java/fr/mrfern/pumpmycord/porg/PorgServerEvent.java
@@ -0,0 +1,13 @@
+package fr.mrfern.pumpmycord.porg;
+
+import net.md_5.bungee.api.event.ServerConnectedEvent;
+import net.md_5.bungee.api.plugin.Listener;
+
+public class PorgServerEvent implements Listener {
+
+	// Appelle de l'event quand serveur connecté et opérationnel
+	public void onServeurConnected(ServerConnectedEvent e) {
+		
+	}
+	
+}
