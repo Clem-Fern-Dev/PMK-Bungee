a123ec717dc3283f586fd3329b6232393844dd0c
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-08 19:26:33 +0100

    Ajout event disconnected & connected server dans class PorgServerEvent

--- src/main/java/fr/mrfern/pumpmycord/porg/PorgServerEvent.java
+++ src/main/java/fr/mrfern/pumpmycord/porg/PorgServerEvent.java
@@ -1,13 +1,33 @@
 package fr.mrfern.pumpmycord.porg;
 
+import fr.mrfern.pumpmycord.Main;
 import net.md_5.bungee.api.event.ServerConnectedEvent;
+import net.md_5.bungee.api.event.ServerDisconnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
 
 public class PorgServerEvent implements Listener {
 
 	// Appelle de l'event quand serveur connecté et opérationnel
+	@EventHandler
 	public void onServeurConnected(ServerConnectedEvent e) {
 		
+		PorgTextChannel porgChan = Main.getMisterP().getPorgTextChannel();
+		
+		porgChan.sendPorgMessage("@everyone " + e.getServer().getInfo().getName() + " est en ligne et opérationnel ! \n Rejoingnez nous en vous connectant via pumpmykins.eu").complete();
+		
 	}
+	
+	// Appelle de l'event quand serveur connecté et opérationnel
+	@EventHandler
+	public void onServeurDisconnected(ServerDisconnectEvent e) {
+			
+		PorgTextChannel porgChan = Main.getMisterP().getPorgTextChannel();
+			
+		porgChan.sendPorgMessage("@everyone " + e.getTarget().getName() + " est hors-ligne ! \n Il sera à nouveau disponible dans les plus brefs délais. Restez informé des nouveautés et mise à jour sur le forum/discord/site !").complete();
+			
+	}
+	
+	
 	
 }
