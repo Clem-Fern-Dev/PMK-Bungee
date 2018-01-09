f83aa97cc56e051a9ea39e709fd3cc71163660b2
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-09 21:26:15 +0100

    Fix message dans PorgServerEvent

--- src/main/java/fr/mrfern/pumpmycord/porg/PorgServerEvent.java
+++ src/main/java/fr/mrfern/pumpmycord/porg/PorgServerEvent.java
@@ -1,25 +1,49 @@
 package fr.mrfern.pumpmycord.porg;
 
 import fr.mrfern.pumpmycord.Main;
-import net.md_5.bungee.api.event.ServerConnectedEvent;
-import net.md_5.bungee.api.event.ServerDisconnectEvent;
-import net.md_5.bungee.api.plugin.Listener;
-import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.config.Configuration;
 
 public class PorgServerEvent {
 	
-	public void OnProxyStartEvent(MisterPorg misterP) {
-		PorgTextChannel porgChan = Main.getMisterP().getPorgTextChannel();
-		
-		porgChan.sendPorgMessage("@everyone **Le réseau PumpMyKins est en ligne et opérationnel !** \nRejoingnez nous en vous connectant via pumpmykins.eu , nous vous remercions pour votre patience !").complete();
+	public void OnProxyStartEvent(MisterPorg misterP) {		
+		try {
+			Configuration config = Main.getConf().getConfiguration("config.yml");
+			
+			PorgTextChannel porgChan = Main.getMisterP().getPorgTextChannel();
+			
+			if(config.getBoolean("discord.debug_mod")) {
+				
+				porgChan.sendPorgMessage("#fix __debug_mod__ **Le réseau PumpMyKins est en ligne et opérationnel !** \nRejoingnez nous en vous connectant via pumpmykins.eu , nous vous remercions pour votre patience !").complete();
+				
+			}else {
+			
+				porgChan.sendPorgMessage("@everyone **Le réseau PumpMyKins est en ligne et opérationnel !** \nRejoingnez nous en vous connectant via pumpmykins.eu , nous vous remercions pour votre patience !").complete();
+				
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void OnProxyStopEvent(MisterPorg misterP) {
-		PorgTextChannel porgChan = misterP.getPorgTextChannel();
 		
-		porgChan.sendPorgMessage("@everyone **Le réseau PumpMyKins est hors-ligne !** \nToute l'équipe PMK fait son possible pour vous redonnez l'accès à ces services au plus vite. Restez informé des nouveautés et mises à jours sur le forum/discord/site !").complete();
+		try {
+			Configuration config = Main.getConf().getConfiguration("config.yml");
+			PorgTextChannel porgChan = misterP.getPorgTextChannel();
+			
+			if(config.getBoolean("discord.debug_mod")) {
+				porgChan.sendPorgMessage("#fix __debug_mod__ **Le réseau PumpMyKins est hors-ligne !** \nToute l'équipe PMK fait son possible pour vous redonnez l'accès à ces services au plus vite. Restez informé des nouveautés et mises à jours sur le forum/discord/site !").complete();
+			}else {
+				porgChan.sendPorgMessage("@everyone **Le réseau PumpMyKins est hors-ligne !** \nToute l'équipe PMK fait son possible pour vous redonnez l'accès à ces services au plus vite. Restez informé des nouveautés et mises à jours sur le forum/discord/site !").complete();
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
-	
-	
 	
 }
