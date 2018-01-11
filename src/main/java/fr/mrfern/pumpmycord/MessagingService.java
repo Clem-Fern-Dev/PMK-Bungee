b73046b4a0f07d4d16c49c0f42422d3159192e31
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-11 22:44:11 +0100

    Ajout subchannel servermanager dans MessagingService event

--- src/main/java/fr/mrfern/pumpmycord/MessagingService.java
+++ src/main/java/fr/mrfern/pumpmycord/MessagingService.java
@@ -1,5 +1,10 @@
 package fr.mrfern.pumpmycord;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import net.md_5.bungee.api.event.PluginMessageEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -7,7 +12,7 @@ import net.md_5.bungee.event.EventHandler;
 public class MessagingService implements Listener {
 	
 	@EventHandler
-	public void OnMessagingServiceReceive(PluginMessageEvent e) {
+	public void OnMessagingServiceReceive(PluginMessageEvent e) throws IOException {
 		String tag = e.getTag();
 		System.out.println(" Message receive " + tag);
 		
@@ -15,6 +20,12 @@ public class MessagingService implements Listener {
 		case "BungeeCord":
 			
 			Main.getMisterP().getPorgTextChannel().sendPorgMessage(" BungeeCord Messaging recieve !").complete();
+			
+			DataInputStream in = new DataInputStream(new ByteArrayInputStream(e.getData()));
+		    String subchannel = in.readUTF();
+		    if (subchannel.equals("servermanager")) {
+		      
+		    }
 			
 			break;
 
