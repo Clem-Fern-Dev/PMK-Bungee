6c191cb8ee9f087157f5570868631a4bf4dc9397
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-17 19:01:01 +0100

    AJout & Update write DataOutput

--- src/main/java/fr/mrfern/pumpmycord/MessagingService.java
+++ src/main/java/fr/mrfern/pumpmycord/MessagingService.java
@@ -30,18 +30,53 @@ public class MessagingService implements Listener {
 				ProxiedPlayer p = Main.getMain().getProxy().getPlayer(e.getReceiver().toString());
 				String playerName = p.getDisplayName();
 				
-				// check server state
+				boolean serverState = ServerManager.getManager(p).getServerState(serverName);
 				
-				ByteArrayOutputStream out = new ByteArrayOutputStream();
-				//DataOutputStream outMessage = new DataOutputStream(out);
-	            //outMessage.writeUTF("prejoinresponse");
-	            //outMessage.writeUTF(serverName);
-	           // outMessage.writeBoolean(false);
+				if(!serverState) {
+					ByteArrayOutputStream out = new ByteArrayOutputStream();
+					DataOutputStream outMessage = new DataOutputStream(out);
+					outMessage.writeUTF("prejoinresponse");
+		            outMessage.writeUTF(serverName);
+		            outMessage.writeBoolean(false);
 				
-	            out.write(165);
-			
-				p.getServer().sendData("BungeeCord", out.toByteArray());
+					p.getServer().sendData("BungeeCord", out.toByteArray());
+				}else {
+					boolean isBan = ServerManager.getManager(p).isBan(serverName);
+					
+					ByteArrayOutputStream out = new ByteArrayOutputStream();
+					DataOutputStream outMessage = new DataOutputStream(out);
+					outMessage.writeUTF("prejoinresponse");
+		            outMessage.writeUTF(serverName);
+		            outMessage.writeBoolean(true);
+					
+					if(isBan) {						
+			            outMessage.writeBoolean(true);
+			            outMessage.writeUTF(ServerManager.getManager(p).getAuthor(serverName));
+			            outMessage.writeUTF(ServerManager.getManager(p).getAuthorUUID(serverName));
+			            
+			            outMessage.writeBoolean(ServerManager.getManager(p).getBanIsGlobal());
+			            
+			            outMessage.writeUTF(ServerManager.getManager(p).getRaison());
+			            
+			            outMessage.writeInt(ServerManager.getManager(p).getDay());
+			            outMessage.writeInt(ServerManager.getManager(p).getHour());
+			            outMessage.writeInt(ServerManager.getManager(p).getMinute());
+			            
+			            outMessage.writeInt(ServerManager.getManager(p).getYear_end());
+			            outMessage.writeInt(ServerManager.getManager(p).getMonth_end());
+			            outMessage.writeInt(ServerManager.getManager(p).getDay_end());
+			            outMessage.writeInt(ServerManager.getManager(p).getHour_end());
+			            outMessage.writeInt(ServerManager.getManager(p).getMinute_end());
+			            
+			            
+			            p.getServer().sendData("BungeeCord", out.toByteArray());
+					}else {
+						outMessage.writeBoolean(false);						
+						p.getServer().sendData("BungeeCord", out.toByteArray());
+					}
+				}
 				
+					
 				
 				break;
 
