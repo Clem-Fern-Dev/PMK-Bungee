4abe114f2306c9de85c373e46ed2183595ec8702
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-20 00:06:10 +0100

    AJout methods update player count & attributs class ServerState

--- src/main/java/fr/mrfern/pumpmycord/server/ServerState.java
+++ src/main/java/fr/mrfern/pumpmycord/server/ServerState.java
@@ -1,5 +1,41 @@
 package fr.mrfern.pumpmycord.server;
 
+import net.md_5.bungee.api.config.ServerInfo;
+
 public class ServerState {
 
+	private int playerCount;
+	private boolean state;
+	private ServerInfo serverInfo;
+	
+	public void updatePlayerCount() {
+		if (serverInfo != null) {
+			playerCount = serverInfo.getPlayers().size();
+		}
+	}
+
+	public int getPlayerCount() {
+		return playerCount;
+	}
+
+	public void setPlayerCount(int playerCount) {
+		this.playerCount = playerCount;
+	}
+
+	public boolean isState() {
+		return state;
+	}
+
+	public void setState(boolean state) {
+		this.state = state;
+	}
+
+	public ServerInfo getServerInfo() {
+		return serverInfo;
+	}
+
+	public void setServerInfo(ServerInfo serverInfo) {
+		this.serverInfo = serverInfo;
+	}
+	
 }
