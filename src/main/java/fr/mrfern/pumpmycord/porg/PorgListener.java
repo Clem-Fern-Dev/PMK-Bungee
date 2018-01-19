e98ecd067e0db9aca67bdb8a0fd5e54abf873b60
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-19 23:56:02 +0100

    Création de la classe

---
+++ src/main/java/fr/mrfern/pumpmycord/porg/PorgListener.java
@@ -0,0 +1,14 @@
+package fr.mrfern.pumpmycord.porg;
+
+import net.dv8tion.jda.core.events.Event;
+import net.dv8tion.jda.core.hooks.EventListener;
+
+public class PorgListener implements EventListener{
+
+	@Override
+	public void onEvent(Event event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
