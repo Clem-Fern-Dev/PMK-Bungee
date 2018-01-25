4dde3fd00642e7059f539eb73896053b49a7a2d1
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-25 22:11:30 +0100

    Fix getter ban author name/UUID

--- src/main/java/fr/mrfern/pumpmycord/server/ServerManager.java
+++ src/main/java/fr/mrfern/pumpmycord/server/ServerManager.java
@@ -106,15 +106,38 @@ public class ServerManager {
 	public String getAuthor(String serverName) {
 		
 		ResultSet listRS = new MySQLConnector().sendQuery("SELECT `ban_ID` FROM `player_ban` WHERE `player_UUID`='" + p.getUniqueId() +"'");	// commande pour récupérer les ids de ban correspondant à ce UUID
-		List<Integer> banIDList = new ArrayList<>();
-		
 		try {
 			// récupération du contenu de la table
 			while(listRS.next()) {
 				try {
 					// ajout à la liste de banID
-					System.out.println(listRS.getInt("ban_ID"));
-					banIDList.add(listRS.getInt("ban_ID"));
+					int banID = listRS.getInt("ban_ID");
+					ResultSet rs = new MySQLConnector().sendQuery("SELECT `author_UUID`, `author_name`, `ban_type`, `raison`, `end` FROM `ban_list` WHERE `id`=" + banID); // récupération de l'UUID du joueur banni
+					rs.next();
+					
+					//String author_UUID = rs.getString("author_UUID");
+					String author_name = rs.getString("author_name");
+					String ban_type = rs.getString("ban_type");
+					//String raison = rs.getString("raison");
+					boolean end = rs.getBoolean("end");
+					
+					if(end) {	// calcul end fait toutes les minutes
+						ResultSet removeRS = new MySQLConnector().sendQuery("SELECT `player_UUID` FROM `player_ban` WHERE `ban_ID`=" + banID); // récupération de l'UUID du joueur banni
+						removeRS.next();
+						String removeUUID = removeRS.getString("player_UUID");						
+						new MySQLConnector().sendUpdate("DELETE FROM `player_ban` WHERE `ban_ID`=" + banID);	// suppresion de la ligne correspondante à cette UUID dans la table player_ban
+						new MySQLConnector().sendUpdate("UPDATE `ban_list` SET `player_UUID`='" + removeUUID + "' WHERE `id`=" + banID);	// update ligne dans ban_list
+					
+						// donc deban / vérification du nom du serveur
+						if(serverName.equals(ban_type) | "global".equals(ban_type)) {
+							return "none";
+						}
+					}else {
+						// sinon ban / vérification du nom du serveur
+						if(serverName.equals(ban_type) | "global".equals(ban_type)) {
+							return author_name;
+						}
+					}					
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
@@ -126,8 +149,47 @@ public class ServerManager {
 	}
 	
 	public String getAuthorUUID(String serverName) {
-		// TODO Auto-generated method stub
-		return "CrazyDoC";
+		ResultSet listRS = new MySQLConnector().sendQuery("SELECT `ban_ID` FROM `player_ban` WHERE `player_UUID`='" + p.getUniqueId() +"'");	// commande pour récupérer les ids de ban correspondant à ce UUID
+		try {
+			// récupération du contenu de la table
+			while(listRS.next()) {
+				try {
+					// ajout à la liste de banID
+					int banID = listRS.getInt("ban_ID");
+					ResultSet rs = new MySQLConnector().sendQuery("SELECT `author_UUID`, `author_name`, `ban_type`, `raison`, `end` FROM `ban_list` WHERE `id`=" + banID); // récupération de l'UUID du joueur banni
+					rs.next();
+					
+					String author_UUID = rs.getString("author_UUID");
+					//String author_name = rs.getString("author_name");
+					String ban_type = rs.getString("ban_type");
+					//String raison = rs.getString("raison");
+					boolean end = rs.getBoolean("end");
+					
+					if(end) {	// calcul end fait toutes les minutes
+						ResultSet removeRS = new MySQLConnector().sendQuery("SELECT `player_UUID` FROM `player_ban` WHERE `ban_ID`=" + banID); // récupération de l'UUID du joueur banni
+						removeRS.next();
+						String removeUUID = removeRS.getString("player_UUID");						
+						new MySQLConnector().sendUpdate("DELETE FROM `player_ban` WHERE `ban_ID`=" + banID);	// suppresion de la ligne correspondante à cette UUID dans la table player_ban
+						new MySQLConnector().sendUpdate("UPDATE `ban_list` SET `player_UUID`='" + removeUUID + "' WHERE `id`=" + banID);	// update ligne dans ban_list
+					
+						// donc deban / vérification du nom du serveur
+						if(serverName.equals(ban_type) | "global".equals(ban_type)) {
+							return "none";
+						}
+					}else {
+						// sinon ban / vérification du nom du serveur
+						if(serverName.equals(ban_type) | "global".equals(ban_type)) {
+							return author_UUID;
+						}
+					}					
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		return "none";
 	}
 
 	public boolean getBanIsGlobal() {
