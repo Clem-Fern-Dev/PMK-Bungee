946ae0f1180653a17d2ad5e11b120608663bb136
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-01-25 02:11:21 +0100

    Fix null delete bdd & fix methods sendQuery/sendUpdate

--- src/main/java/fr/mrfern/pumpmycord/server/ServerManager.java
+++ src/main/java/fr/mrfern/pumpmycord/server/ServerManager.java
@@ -1,8 +1,13 @@
 package fr.mrfern.pumpmycord.server;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import fr.mrfern.pumpmycord.Response;
+import fr.mrfern.pumpmycord.config.MySQLConnector;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class ServerManager {
@@ -46,7 +51,53 @@ public class ServerManager {
 	}
 
 	public boolean isBan(String serverName) {
-		// TODO Auto-generated method stub
+		System.out.println(p.getUniqueId());
+		ResultSet listRS = new MySQLConnector().sendQuery("SELECT `ban_ID` FROM `player_ban` WHERE `player_UUID`='" + p.getUniqueId() +"'");	// commande pour récupérer les ids de ban correspondant à ce UUID
+		List<Integer> banIDList = new ArrayList<>();
+		
+		try {
+			// récupération du contenu de la table
+			while(listRS.next()) {
+				try {
+					// ajout à la liste de banID
+					System.out.println(listRS.getInt("ban_ID"));
+					banIDList.add(listRS.getInt("ban_ID"));
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}			
+			if(banIDList.isEmpty()) {
+				// si pas de ban avec cette uuid alors return false
+				System.out.println("pas de ban, liste vide");
+				return false;
+			}else {
+				for (int banID : banIDList) {
+					ResultSet banRS = new MySQLConnector().sendQuery("SELECT `author_UUID`, `author_name`, `ban_type`, `end` FROM `ban_list` WHERE id=" + banID);
+					banRS.next();
+					
+					if(banRS.getBoolean("end")) {	// calcul end fait toutes les minutes
+						ResultSet removeRS = new MySQLConnector().sendQuery("SELECT `player_UUID` FROM `player_ban` WHERE `ban_ID`=" + banID); // récupération de l'UUID du joueur banni
+						removeRS.next();
+						String removeUUID = removeRS.getString("player_UUID");						
+						new MySQLConnector().sendUpdate("DELETE FROM `player_ban` WHERE `ban_ID`=" + banID);	// suppresion de la ligne correspondante à cette UUID dans la table player_ban
+						new MySQLConnector().sendUpdate("UPDATE `ban_list` SET `player_UUID`='" + removeUUID + "' WHERE `id`=" + banID);	// update ligne dans ban_list
+					
+						// donc deban / vérification du nom du serveur
+						if(serverName.equals(banRS.getString("ban_type"))) {
+							return false;
+						}
+					}else {
+						// sinon ban
+						return true;
+					}
+					
+				}
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return false;
 	}
 	
